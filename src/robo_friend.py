8c176aaee0c4a22b9240be64f0184fb44b10da8b
arontrez03 <arontrez03@gmail.com>
2020-03-09 22:09:14 +0800

    Update robo_friend.py

--- src/robo_friend.py
+++ src/robo_friend.py
@@ -12,6 +12,33 @@ import numpy as np
 import random
 import string # to process standard python strings
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def greeting(sentence):
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES)
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+def response(user_response):
+    robo_response=''
+    sent_tokens.append(user_response)
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx=vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if(req_tfidf==0):
+        robo_response=robo_response+"I am sorry! I don't understand you"
+        return robo_response
+    else:
+        robo_response = robo_response+sent_tokens[idx]
+        return robo_response
 
 f=open('chatbot.txt','r',errors = 'ignore')
 raw=f.read()
@@ -20,22 +47,34 @@ raw=raw.lower()# converts to lowercase
 sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
 word_tokens = nltk.word_tokenize(raw)# converts to list of words
 
-print(word_tokens[1])
-print("Type of sent_tokens:({})".format(type(sent_tokens)))
-print("Type of word_tokens:({})".format(type(word_tokens)))
+print(word_tokens[1]) #For debug purposes only
+print("Type of sent_tokens:({})".format(type(sent_tokens))) #For debug purposes only
+print("Type of word_tokens:({})".format(type(word_tokens))) #For debug purposes only
 
 lemmer = nltk.stem.WordNetLemmatizer()
 #WordNet is a semantically-oriented dictionary of English included in NLTK.
-def LemTokens(tokens):
-    return [lemmer.lemmatize(token) for token in tokens]
-remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
-def LemNormalize(text):
-    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
 
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
 GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
 GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
-def greeting(sentence):
- 
-    for word in sentence.split():
-        if word.lower() in GREETING_INPUTS:
-            return random.choice(GREETING_RESPONSES)
+print("Type of GREETINGS_INPUTS:({})".format(type(GREETING_INPUTS)))
+print("Type of GREETING_RESPONSES:({})".format(type(GREETING_RESPONSES)))
+flag=True
+print("ROBO: My name is Robo. I will answer your queries about Chatbots. If you want to exit, type Bye!")
+while(flag==True):
+    user_response = input()
+    user_response=user_response.lower()
+    if(user_response!='bye'):
+        if(user_response=='thanks' or user_response=='thank you' ):
+            flag=False
+            print("ROBO: You are welcome..")
+        else:
+            if(greeting(user_response)!=None):
+                print("ROBO: "+greeting(user_response))
+            else:
+                print("ROBO: ",end="")
+                print(response(user_response))
+                sent_tokens.remove(user_response)
+    else:
+        flag=False
+        print("ROBO: Bye! take care..")
